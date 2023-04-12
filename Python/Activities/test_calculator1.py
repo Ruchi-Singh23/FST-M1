6912dcecd0de52b2c3e0aaacdc02e454f909f366
Ruchi-Singh23 <129042238+Ruchi-Singh23@users.noreply.github.com>
2023-04-12 17:29:19 +0530

    Add files via upload

---
+++ Python/Activities/test_calculator1.py
@@ -0,0 +1,26 @@
+import pytest
+import math
+
+
+def test_add():
+    num1=25
+    num2=30
+    assert num1 + num2 == 55
+
+
+def test_sub():
+    num1=8
+    num2=4
+    assert num1 - num2 == 4
+
+
+def test_quot():
+    num1=4
+    num2=2
+    assert math.exp()
+
+
+def test_mult():
+    num1=6
+    num2=3
+    assert num1 * num2 == 18
